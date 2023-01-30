2dd700e70f03838c515c6d8ac90700816b8665c7
Jack <37642239+jackintoshplus@users.noreply.github.com>
2023-01-29 22:42:22 -0800

    Add files via upload

---
+++ higherlower.py
@@ -0,0 +1,71 @@
+#Higher or Lower
+#
+#Application created by Jack Herbert - 1/29/2023
+
+#Library import
+import random
+
+#Application value declarations
+activeGame = True;
+wins = 0;
+
+#Application greeter
+print('Higher or Lower\n')
+
+#Main application loop
+while activeGame:
+
+    #Set player values and reset locks.
+    resetCheckActive = True;
+    compareCheckActive = True;
+    userInputCheckActive = True;
+    usernum = 0
+    battlenum = random.randint(1,100)
+
+    #Application begins
+    print('Prepare to battle! Current wins:', wins)
+    print('Choose a number between 1 and 100.\n')
+
+    #User input    
+    while(userInputCheckActive):       
+        try:
+            usernum = input('Number: ')
+            usernum = int(usernum)
+        except ValueError:
+            print("Invalid entry. Please try again.")
+            continue
+        else:
+            break
+
+    #User Higher or Lower check
+    while(compareCheckActive):
+        usercompare = input('Will it be Higher or Lower:')      
+        usercompare.lower 
+
+        if((usercompare == 'higher') or (usercompare == 'lower')):
+            compareCheckActive = False
+        else:
+            print("Invalid entry. Please try again")
+            
+    #User comparisons/Battle
+    print('You believe', usernum, 'is', usercompare, 'than', battlenum)
+    
+    if((usernum > battlenum and usercompare == 'higher') or (usernum < battlenum and usercompare == 'lower')):
+        print('You won!')
+        wins += 1
+    else:
+        print('You lost...')
+
+    #Re-lock reset game checker
+    resetCheckActive = True;
+
+    #Allow user to replay game or exit
+    while resetCheckActive:
+        resetCheck = input('Play again? (Y/N):')
+
+        if((resetCheck == 'y')) or (resetCheck == 'n'):
+            resetCheckActive = False
+        else:
+            print("Invalid entry. Please try again")
+
+quit();
